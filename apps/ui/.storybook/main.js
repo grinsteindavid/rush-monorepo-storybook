96d688526d8e05c6d05146ddb8be8456ed27e022
david miranda <davidmirandagrinstein@gmail.com>
2022-05-03 17:37:15 -0400

    storybook works with npm only

--- apps/ui/.storybook/main.js
+++ apps/ui/.storybook/main.js
@@ -1,15 +1,20 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  async viteFinal(config, { configType }) {
+    const { dirname } = require("path");
+
+    // https://github.com/eirslett/storybook-builder-vite/issues/55
+    config.root = dirname(require.resolve("@storybook/builder-vite"));
+    // config.server.fsServe = {};
+    return config;
+  },
+};
