19897fca548c2224da2024412248647355c50770
david miranda <davidmirandagrinstein@gmail.com>
2022-05-08 01:41:58 -0400

    working

--- apps/ui/.storybook/main.js
+++ apps/ui/.storybook/main.js
@@ -1,20 +1,16 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
-  },
-  async viteFinal(config, { configType }) {
-    const { dirname } = require("path");
-
-    // https://github.com/eirslett/storybook-builder-vite/issues/55
-    config.root = dirname(require.resolve("@storybook/builder-vite"));
-    // config.server.fsServe = {};
-    return config;
-  },
 };
